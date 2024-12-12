1ab8c5819aaa5f2f59f0ea64f79e91ab7ca94fbb
Sandro Costa <sandro.nbs@gmail.com>
2024-12-12 21:15:25 +0000

    Add neotree

--- init.lua
+++ init.lua
@@ -941,6 +941,7 @@ require('lazy').setup({
   require 'kickstart.plugins.codeium',
   require 'kickstart.plugins.oil',
   require 'kickstart.plugins.nvim-surround',
+  require 'kickstart.plugins.neotree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
---
+++ lua/kickstart/plugins/neotree.lua
@@ -0,0 +1,14 @@
+-- neotree.lua
+--
+-- File explorer that lets you edit your filesystem like a normal Neovim buffer
+--
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+}
